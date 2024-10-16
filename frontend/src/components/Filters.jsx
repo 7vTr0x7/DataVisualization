c22e1eada96a76396e5610aa7fb33e3c4b33ed29
7vTr0x7 <7vTrox7@gmail.com>
2024-10-16 16:39:31 +0530

    Filters

---
+++ frontend/src/components/Filters.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const data = {
+  sday: "2022-04-10",
+  eday: "2022-10-10",
+  age: "15-25",
+  gender: "Male",
+  a: 550,
+  b: 92,
+  c: 413,
+  d: 988,
+  e: 709,
+  f: 175,
+};
+
+const Filters = () => {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [gender, setGender] = useState(null);
+  const [age, setAge] = useState(null);
+
+  useEffect(() => {
+    setStartDate(data.sday);
+    setEndDate(data.eday);
+  }, [data]);
+
+  return (
+    <div className="flex gap-4">
+      <div>
+        <select
+          className="border border-black font-semibold px-3 rounded-sm"
+          disabled={data}
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+      </div>
+      <div>
+        <select
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          className="border border-black font-semibold px-3 rounded-sm"
+          disabled={data}>
+          <option value="15-25">15-25</option>
+          <option value=">25">{">25"}</option>
+        </select>
+      </div>
+      <div>
+        <DatePicker
+          disabled={data}
+          className="border text-black border-black font-semibold px-3 rounded-sm"
+          selected={startDate}
+          onChange={(dates) => {
+            const [start, end] = dates;
+            setStartDate(start);
+            setEndDate(end);
+          }}
+          selectsRange
+          startDate={startDate}
+          endDate={endDate}
+          isClearable
+          placeholderText="Select a date range"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Filters;
+
+{
+  /* <div>
+  <label htmlFor="startDate" className="font-semibold px-3">
+    Start:
+  </label>
+  <input
+    type="date"
+    className="border border-black font-semibold px-3 rounded-sm"
+    value={startDate}
+    onChange={(e) => setStartDate(e.target.value)}
+  />
+</div>; */
+}
+{
+  /* <div>
+<label htmlFor="endDate" className="font-semibold px-3">
+  End:
+</label>
+<input
+  type="date"
+  className="border border-black font-semibold px-3 rounded-sm"
+  value={endDate}
+  onChange={(e) => setEndDate(e.target.value)}
+/>
+</div> */
+}
--- frontend/src/pages/Charts.jsx
+++ frontend/src/pages/Charts.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Filters from "../components/Filters";
 
 const Charts = () => {
-  return <div></div>;
+  return (
+    <div className="flex justify-center items-center h-56 ">
+      <Filters />
+    </div>
+  );
 };
 
 export default Charts;
