bb398b8da5c4400e079db1eb1ad73b732b523a87
7vTr0x7 <7vTrox7@gmail.com>
2024-10-18 18:35:11 +0530

    registerUser

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { registerUser } from "../controllers/user";
 
 const router = express.Router();
 
 router.post("/login");
-router.post("/register");
+router.post("/register", registerUser);
 router.post("/logout");
 
 export default router;
