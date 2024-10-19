6087fb1eabe0ccaf51fd0749203cfcb5e4db070b
7vTr0x7 <7vTrox7@gmail.com>
2024-10-19 16:39:14 +0530

    getChartData

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,6 @@
 import { VisualizationUser } from "../models/user.model.js";
 import bcrypt from "bcrypt";
-import { sendCookie } from "../utils/features.js";
+import { getData, sendCookie } from "../utils/features.js";
 
 export const registerUser = async (req, res) => {
   try {
@@ -24,23 +24,27 @@ export const registerUser = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  let user = await VisualizationUser.findOne({ email }).select("+password");
+    let user = await VisualizationUser.findOne({ email }).select("+password");
 
-  if (!user)
-    return res
-      .status(400)
-      .json({ success: false, message: "Invalid Email or Password" });
+    if (!user)
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Email or Password" });
 
-  const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
 
-  if (!isMatch)
-    return res
-      .status(400)
-      .json({ success: false, message: "Invalid Email or Password" });
+    if (!isMatch)
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Email or Password" });
 
-  sendCookie(user, res, "Login Successfully", 201);
+    sendCookie(user, res, "Login Successfully", 201);
+  } catch (error) {
+    res.status(500).json(`failed to register ${error}`);
+  }
 };
 
 export const getUser = async (req, res) => {
@@ -48,12 +52,29 @@ export const getUser = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  res
-    .status(201)
-    .cookie("token", "", {
-      expires: new Date(Date.now()),
-      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
-      secure: process.env.NODE_ENV === "Development" ? false : true,
-    })
-    .json({ success: true, message: "Logout Successfully" });
+  try {
+    res
+      .status(201)
+      .cookie("token", "", {
+        expires: new Date(Date.now()),
+        sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+        secure: process.env.NODE_ENV === "Development" ? false : true,
+      })
+      .json({ success: true, message: "Logout Successfully" });
+  } catch (error) {
+    res.status(500).json(`failed to register ${error}`);
+  }
+};
+
+export const getChartData = async (req, res) => {
+  try {
+    const data = await getData();
+    if (data.length > 0) {
+      res.status(400).json({ success: true, data });
+    } else {
+      res.status(404).json({ success: false, message: "Data not found" });
+    }
+  } catch (error) {
+    res.status(500).json(`failed to register ${error}`);
+  }
 };
--- backend/utils/features.js
+++ backend/utils/features.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+const Papa = require("papaparse");
 
 export const sendCookie = (user, res, message, statusCode) => {
   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
@@ -12,4 +13,14 @@ export const sendCookie = (user, res, message, statusCode) => {
       secure: process.env.NODE_ENV === "Development" ? false : true,
     })
     .json({ success: true, message, token });
+};
+
+export const getData = async () => {
+  const res = await fetch(
+    `https://docs.google.com/spreadsheets/d/1l7GstWHc69HPV0irSdvoMIyHgtufUPKsbtCiNw7IKR0/export?format=csv&gid=485741054`
+  );
+
+  const csvData = await res.text();
+  const parsedData = Papa.parse(csvData, { header: true });
+  return parsedData.data;
 };
