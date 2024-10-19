39df8f1a0ae2cad3bd0583de6c4c52d3bc6701b7
7vTr0x7 <7vTrox7@gmail.com>
2024-10-19 18:58:18 +0530

    fix

--- frontend/src/components/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const ProtectedRoutes = () => {
   const token = localStorage.getItem("token");
+  const location = useLocation();
 
-  return token ? <Outlet /> : <Navigate to="/login" />;
+  return token ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} />
+  );
 };
 
 export default ProtectedRoutes;
