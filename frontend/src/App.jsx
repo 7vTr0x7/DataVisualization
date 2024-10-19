6cfcd4fa1e9df81deb32b69a54d2e1c1651efa4c
7vTr0x7 <7vTrox7@gmail.com>
2024-10-19 15:29:25 +0530

    ProtectedRoutes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import Charts from "./pages/Charts";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Charts />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/" element={<Charts />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
         </Routes>
       </Router>
     </>
